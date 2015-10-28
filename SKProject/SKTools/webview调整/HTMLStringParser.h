4d04f2aeb01f12edd04d55141bbc0c7655ebf9e6
shaveKevin <shaveKevin@163.com>
2015-10-28 11:13:31 +0800

    web view 调整纠错
    用法说明纠错

--- SKProject/SKTools/webview调整/HTMLStringParser.h
+++ SKProject/SKTools/webview调整/HTMLStringParser.h
@@ -12,6 +12,10 @@
 /**
  
  *   使用方法
+ NSString *urlString = [DetailViewAPI stringByAppendingString:self.url];
+ NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:urlString]];
+ [self.webView loadRequest:request];
+ NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
  NSString *htmlString = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
  htmlString = [[HTMLStringParser sharedInstance] parseWithOriginalHTMLString:htmlString];
  [_webView loadHTMLString:htmlString baseURL:nil];
