de399d21f6b2417572627d7d333d94f94c6f8547
houzhiwei <houzhiwei@allinmd.cn>
2016-06-27 16:37:49 +0800

    ip
    ip

---
+++ SKProject/SKTools/NSString+Category/NSString+getIPAddress.h
@@ -0,0 +1,14 @@
+//
+//  NSString+getIPAddress.h
+//  shaveKevin
+//
+//  Created by shaveKevin on 15/3/2.
+//  Copyright (c) 2015年 shaveKevin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (getIPAddress)
++ (NSString *)getIPAddress;
+
+@end
